5b10de2e5c8434440c9055867be0526c0fe7fc87
MrK0RN <147444432+MrK0RN@users.noreply.github.com>
2023-11-26 07:49:25 +0300

    Delete get_info.php

--- get_info.php
+++
@@ -1,9 +0,0 @@
-<?php
-$private_key = filter_var(trim($_GET['id']),FILTER_SANITIZE_STRING);
-$zz = filter_var(trim($_GET['inf']),FILTER_SANITIZE_STRING);
-$FamN = filter_var(trim($_GET['FamN']),FILTER_SANITIZE_STRING);
-$SecN = filter_var(trim($_GET['SecN']),FILTER_SANITIZE_STRING);
-$FatN = filter_var(trim($_GET['FatN']),FILTER_SANITIZE_STRING);
-$BD = filter_var(trim($_GET['BD']),FILTER_SANITIZE_STRING);
-$prof = filter_var(trim($_GET['prof']),FILTER_SANITIZE_STRING);
-
